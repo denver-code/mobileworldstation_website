f8824e7967a0035044eb5b70d5547e45a87cf80e
denver-code <csigorek@gmail.com>
2023-03-28 16:01:39 +0100

    init

---
+++ components/hero/Hero.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="bg-blue p-10 lg:p-36">
+    <h1 class="text-white text-4xl mb-5">
+      MobileWorldStation - Your best solution to buy a used or new phone.
+    </h1>
+    <h2 class="text-white text-2xl">
+      Bla bla bla
+    </h2>
+  </div>
+</template>
