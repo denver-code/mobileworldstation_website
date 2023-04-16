c1f548d061d3c36dd4f660faa4402bd8447af6db
denver-code <csigorek@gmail.com>
2023-04-16 19:58:28 +0100

    temp fix

---
+++ pages/product_detail/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <VmSubheader />
+    <VmProductsList />
+  </div>
+</template>
+
+<script>
+import VmProductsList from '@/components/products_list/ProductsListContainer';
+import VmHero from '@/components/hero/Hero';
+import VmSubheader from '@/components/subheader/Subheader'
+
+export default {
+  name: 'index',
+  components: {
+    VmProductsList,
+    VmSubheader
+  }
+};
+</script>
